1f7ed5b5bbe97a6ef13351ee69a09dd7b3fd4167
nibble0101 <mj.mawa@yahoo.com>
2021-04-07 09:47:04 +0300

    Add HTML file to index route and fix formatting

--- src/scraper.js
+++ src/scraper.js
@@ -6,25 +6,26 @@ require("dotenv").config();
 const redisConfig = {
   port: process.env.DATABASE_PORT,
   host: process.env.DATABASE_HOST,
-  password: process.env.DATABASE_PASSWORD
-}
+  password: process.env.DATABASE_PASSWORD,
+};
 
 const redis = new Redis(redisConfig);
-
 
 const fetchTrivia = async () => {
   try {
     const response = await redis.flushall();
-    if(response === "OK"){
+    if (response === "OK") {
       console.log(`${getDate()}: Successfully flushed database`);
     }
-    const { overall } = (await axios.get(
-      process.env.URL_GLOBAL_TRIVIA_COUNT
-    )).data;
+    const { overall } = (
+      await axios.get(process.env.URL_GLOBAL_TRIVIA_COUNT)
+    ).data;
     console.log(`${getDate()}: Successfully fetched  overall trivia count`);
     const { token } = (await axios.get(process.env.URL_SESSION_TOKEN)).data;
     console.log(`${getDate()}: Successfully fetched  session token`);
-    const { trivia_categories } = (await axios.get(process.env.URL_QUIZ_CATEGORY_ID)).data;
+    const { trivia_categories } = (
+      await axios.get(process.env.URL_QUIZ_CATEGORY_ID)
+    ).data;
     console.log(`${getDate()}: Successfully fetched  trivia categories`);
     const trivia = [];
 
@@ -34,7 +35,6 @@ const fetchTrivia = async () => {
     while (totalQuizCount > 50) {
       const url = `${process.env.URL_ALL_QUIZ_CATEGORIES}?amount=50&token=${token}`;
       const response = (await axios.get(url)).data;
-      
 
       if (response.response_code === 0) {
         trivia.push(...response.results);
@@ -43,7 +43,7 @@ const fetchTrivia = async () => {
         console.log(`${getDate()}: Failed to fetch trivia ${count}`);
       }
       totalQuizCount -= 50;
-      count++
+      count++;
     }
 
     if (totalQuizCount) {
@@ -55,13 +55,11 @@ const fetchTrivia = async () => {
       } else {
         console.log(`${getDate()} Failed to fetch trivia data ${count + 1}`);
       }
-
-
     }
 
     const filteredTriviaObject = {};
 
-    trivia.forEach(triviaObject => {
+    trivia.forEach((triviaObject) => {
       if (filteredTriviaObject[triviaObject.category] === undefined) {
         filteredTriviaObject[triviaObject.category] = [triviaObject];
       } else {
@@ -71,29 +69,50 @@ const fetchTrivia = async () => {
 
     const categories = Object.keys(filteredTriviaObject);
 
-    for(let categoryIndex = 0; categoryIndex < categories.length; categoryIndex++){
+    for (
+      let categoryIndex = 0;
+      categoryIndex < categories.length;
+      categoryIndex++
+    ) {
       const category = categories[categoryIndex];
       const categoryTrivia = filteredTriviaObject[category];
-      const retrievedCategoryObject = trivia_categories.find(categoryObject => categoryObject.name === category);
-      const { id, name, difficulty } = getDifficultyLevel(retrievedCategoryObject, categoryTrivia);
-      
+      const retrievedCategoryObject = trivia_categories.find(
+        (categoryObject) => categoryObject.name === category
+      );
+      const { id, name, difficulty } = getDifficultyLevel(
+        retrievedCategoryObject,
+        categoryTrivia
+      );
+
       const difficultyQuizPairsArray = Object.entries(difficulty);
 
-      for(let pairIndex = 0; pairIndex < difficultyQuizPairsArray.length; pairIndex++){
-        const [difficultyLevel, listOfQuestions] = difficultyQuizPairsArray[pairIndex];
-        try{
-          await redis.set(`category-${id}-difficulty-${difficultyLevel}`, JSON.stringify(listOfQuestions));
-          console.log(`${getDate()}: Successfully saved ${name} of difficulty ${difficultyLevel} `);
-        }catch(err){
-          console.log(`${getDate()}: Failed to save ${name} of difficulty ${difficultyLevel} ${err}`);
+      for (
+        let pairIndex = 0;
+        pairIndex < difficultyQuizPairsArray.length;
+        pairIndex++
+      ) {
+        const [difficultyLevel, listOfQuestions] = difficultyQuizPairsArray[
+          pairIndex
+        ];
+        try {
+          await redis.set(
+            `category-${id}-difficulty-${difficultyLevel}`,
+            JSON.stringify(listOfQuestions)
+          );
+          console.log(
+            `${getDate()}: Successfully saved ${name} of difficulty ${difficultyLevel} `
+          );
+        } catch (err) {
+          console.log(
+            `${getDate()}: Failed to save ${name} of difficulty ${difficultyLevel} ${err}`
+          );
         }
       }
 
-      if(categoryIndex === categories.length - 1){
+      if (categoryIndex === categories.length - 1) {
         redis.disconnect();
         console.log(`${getDate()}: Successfully disconnected from database`);
       }
-
     }
   } catch (err) {
     console.log(`${getDate()}: An error has occurred`, err);
@@ -101,8 +120,3 @@ const fetchTrivia = async () => {
 };
 
 fetchTrivia();
-
-
-
-
-
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,18 @@
 function getDifficultyLevel(categoryObject, listOfQuestions) {
-    const { id, name } = categoryObject;
-    const finalQuizObject = { id, name, difficulty: { mixed: listOfQuestions, easy: [], medium: [], hard: [] } };
-    listOfQuestions.forEach((quizObject => {
-        finalQuizObject.difficulty[quizObject.difficulty].push(quizObject);
-    }))
-    return finalQuizObject;
+  const { id, name } = categoryObject;
+  const finalQuizObject = {
+    id,
+    name,
+    difficulty: { mixed: listOfQuestions, easy: [], medium: [], hard: [] },
+  };
+  listOfQuestions.forEach((quizObject) => {
+    finalQuizObject.difficulty[quizObject.difficulty].push(quizObject);
+  });
+  return finalQuizObject;
 }
 
 function getDate() {
-    return new Date().toISOString();
+  return new Date().toISOString();
 }
 
 module.exports = { getDifficultyLevel, getDate };
