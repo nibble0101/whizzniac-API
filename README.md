5803e0995fe7fbeae1496c6a01f01160c7c345a0
nibble0101 <mj.mawa@yahoo.com>
2021-04-07 09:34:20 +0300

    Add HTML file to index route

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Whizzniac API
 
-This is an API for whizzniac quiz app
+This is an API for [whizzniac quiz app](https://whizzniac.netlify.app/). It was built using [Express.js](https://expressjs.com/) and deployed to heroku.
+
+## Live project
+
+The live project can be viewed [here](https://whizzniac-api.herokuapp.com/).
--- server.js
+++ server.js
@@ -7,31 +7,34 @@ server.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 const redisConfig = {
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
-    password: process.env.DATABASE_PASSWORD
-  }
+  port: process.env.DATABASE_PORT,
+  host: process.env.DATABASE_HOST,
+  password: process.env.DATABASE_PASSWORD,
+};
 const redis = new Redis(redisConfig);
 
-
 server.get("/", (request, response) => {
-   response.json({"message": "Hello world!"});
+  response.sendFile(__dirname + "/public/index.html");
 });
 
 server.get("/trivia/:category/:difficulty", async (req, res) => {
-    const { category, difficulty } = req.params;
-    if (!category.trim() || !difficulty.trim()) {
-        res.status(400).send({ "message": "Bad request" });
-        return;
-    }
-    const response = await redis.get(`category-${category.trim()}-difficulty-${difficulty.trim()}`);
-    if (!response) {
-        res.status(404).send({ "message": "Resource not found" });
-        return;
-    }
-    res.status(200).send({ data: JSON.parse(response) });
-})
+  const { category, difficulty } = req.params;
+  if (!category.trim() || !difficulty.trim()) {
+    res.status(400).send({ message: "Bad request" });
+    return;
+  }
+  const response = await redis.get(
+    `category-${category.trim()}-difficulty-${difficulty.trim()}`
+  );
+  if (!response) {
+    res.status(404).send({ message: "Resource not found" });
+    return;
+  }
+  res.status(200).send({ data: JSON.parse(response) });
+});
+
+server.use(express.static("public"));
 
 server.listen(PORT, () => {
-    console.log(`Your app is listening on port ${PORT}`);
+  console.log(`Your app is listening on port ${PORT}`);
 });
